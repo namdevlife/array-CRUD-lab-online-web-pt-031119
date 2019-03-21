bb8922a56e32cc767dc807a30e5e175930b68743
NAM NGUYEN <namdevlife@gmail.com>
2019-03-21 04:37:17 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  items = ["car", "house", "water", "cool"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  .push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  .unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  .pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  .shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  [2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  [-1]
 end
