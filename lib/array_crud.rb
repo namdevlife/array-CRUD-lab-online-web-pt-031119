963329ffe0d99c98bc78b18d4be595b25fe6d72a
NAM NGUYEN <namdevlife@gmail.com>
2019-03-21 04:33:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  .push(element)
+  items.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
@@ -15,21 +15,21 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop(element)
+  .pop(element)
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift(element)
+  .shift(element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  retrieve_element_from_index[index_number]
+  [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  retrieve_first_element_from_array[0]
+  [0]
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last_element_from_array[-1]
+  [-1]
 end
